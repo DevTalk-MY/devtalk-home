80046a8b7707fe1ec16cd02ba32939a38b735265
nrmnqdds <quddusnuriman@gmail.com>
2024-06-14 20:25:48 +0800

    :bug: turns landing page into ssr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,52 +1,60 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+	src: "./fonts/GeistVF.woff",
+	variable: "--font-geist-sans",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+	src: "./fonts/GeistMonoVF.woff",
+	variable: "--font-geist-mono",
 });
 
 export const metadata: Metadata = {
-  title: "DevTalk Home",
-  description: "Virtual gathering place for developers",
-  icons: [
-    {
-      rel: "icon",
-      type: "image/svg+xml",
-      url: "/assets/favicon.svg",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      url: "/assets/favicon.png",
-    },
-  ],
-  openGraph: {
-    images: ['https://devtalk.club/assets/og-image.png'],
-  },
-  twitter: {
-    images: ['https://devtalk.club/assets/og-image.png'],
-  }
+	title: "DevTalk Home",
+	description: "Virtual gathering place for developers",
+	icons: [
+		{
+			rel: "icon",
+			type: "image/svg+xml",
+			url: "/assets/favicon.svg",
+		},
+		{
+			rel: "icon",
+			type: "image/png",
+			url: "/assets/favicon.png",
+		},
+	],
+	openGraph: {
+		images: ["https://devtalk.club/assets/og-image.png"],
+	},
+	twitter: {
+		images: ["https://devtalk.club/assets/og-image.png"],
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <div id="hidden-message" className="hidden">
-          If you are looking at this, yes, I am doing a simple design for now. I will update this will a full design soon.
-        </div>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${geistSans.variable} ${geistMono.variable}`}>
+				<div id="hidden-message" className="hidden">
+					If you are looking at this, yes, I am doing a simple design for now. I
+					will update this will a full design soon.
+				</div>
+				{children}
+				<Script>
+					console.log("If you are looking at this, yes, I am doing a simple
+					design for now. I will update this will a full design soon.");
+					console.log("You can also contribute (if you will) at
+					https://github.com/devtalk-my/devtalk-home");
+				</Script>
+			</body>
+		</html>
+	);
 }
